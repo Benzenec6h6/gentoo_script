8f25b87ee2e1f9add63834e0426ccf84319f1858
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-07-23 15:10:20 +0900

    renew

--- profile/kernel.sh
+++ profile/kernel.sh
@@ -2,14 +2,9 @@
 set -euo pipefail
 source ./00-env.sh
 
-cat <<EOF | chroot $MOUNTPOINT /bin/bash
-set -euo pipefail
-
 emerge --quiet sys-kernel/gentoo-sources sys-kernel/genkernel
 
 # genkernel を使ってカーネルビルド（オプション: all または menuconfig）
 genkernel --install all
-
-EOF
 
 echo "[+] Kernel built using genkernel."
