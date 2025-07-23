3df71cb41b62a8ae09e5fb0de64f65b979c7a989
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-07-23 15:04:08 +0900

    delete old scripts

--- 11-cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-source ./00-env.sh
-
-swapoff ${DISK}2 || true
-umount -R $MOUNTPOINT || true
-
-echo "[✓] Gentoo installation completed. You may now reboot."
