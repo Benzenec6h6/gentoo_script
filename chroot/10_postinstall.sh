f82335fece8a10fe30a16ccdd577e11849d21309
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-28 17:28:23 +0900

    add package networkmanager fastfetch delete dotfiles

--- chroot/10_postinstall.sh
+++ chroot/10_postinstall.sh
@@ -12,16 +12,21 @@ emerge --verbose \
   x11-misc/tint2 \
   x11-misc/picom \
   lxappearance \
+  net-misc/networkmanager \
   network-manager-applet \
+  app-misc/fastfetch \
   git
 
+
+rc-update add NetworkManager default
+
 # ユーザー用ホームディレクトリに dotfiles クローン
-sudo -u $USERNAME bash <<'INNER'
-cd ~
-git clone --depth 1 https://github.com/Benzenec6h6/dotfiles.git
-cp -r dotfiles/gentoo_dot/. ~/
-rm -rf dotfiles
-INNER
+#sudo -u $USERNAME bash <<'INNER'
+#cd ~
+#git clone --depth 1 https://github.com/Benzenec6h6/dotfiles.git
+#cp -r dotfiles/gentoo_dot/. ~/
+#rm -rf dotfiles
+#INNER
 
 # xinitrc
 echo "exec openbox-session" > /home/$USERNAME/.xinitrc
