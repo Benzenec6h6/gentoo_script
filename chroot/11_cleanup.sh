13e9b4628a2d9ed22fa696b71575c04b80c0d47c
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-28 17:29:54 +0900

    fixed swapoff diskname and umount destination

--- chroot/11_cleanup.sh
+++ chroot/11_cleanup.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source "$SCRIPT_DIR/00_env.sh"
 
-swapoff ${DISK}2 || true
-umount -R $MOUNTPOINT || true
+swapoff "$DISK_SWAP" || true
+umount -R "$MOUNTPOINT"/{proc,sys,dev} 2>/dev/null || true
+umount -R "$MOUNTPOINT" || true
 
 echo "[✓] Gentoo installation completed. You may now reboot."
