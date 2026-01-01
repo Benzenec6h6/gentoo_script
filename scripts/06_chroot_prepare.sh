87aea7fc9686d94b5004d48fcb226252248456cb
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-01-02 01:16:25 +0900

    delete profile dir

--- scripts/06_chroot_prepare.sh
+++ scripts/06_chroot_prepare.sh
@@ -8,14 +8,7 @@ echo "[+] Preparing for chroot..."
 cp --dereference /etc/resolv.conf "$MOUNTPOINT/etc/"
 cp -r ./chroot "$MOUNTPOINT/chroot"
 cp -r ./assets "$MOUNTPOINT/assets"
-cp -r ./profile "$MOUNTPOINT/profile"
 cp -r ./00_env.sh "$MOUNTPOINT/00_env.sh"
-
-#mount "${DISK_ROOT}" "$MOUNTPOINT"             # 二重マウントになっている可能性ありコメントアウトで様子見
-#mount --mkdir "${DISK_BOOT}" "$MOUNTPOINT/boot" # /boot が別パーティションの場合
-#if [[ -d /sys/firmware/efi ]]; then
-#    mount --mkdir "${DISK_BOOT}" "$MOUNTPOINT/efi"  # ここも二重マウントのときと同様03の内容とかぶっている
-#fi
 
 mount --types proc /proc "$MOUNTPOINT/proc"
 mount --rbind /sys "$MOUNTPOINT/sys"
