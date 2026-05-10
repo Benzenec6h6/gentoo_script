45303b92bf5c201b6f9054b565c174e43512aa0a
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-05-10 22:51:01 +0900

    Gather the necessary tools

--- chroot/10_postinstall.sh
+++ chroot/10_postinstall.sh
@@ -5,29 +5,10 @@ source "$SCRIPT_DIR/00_env.sh"
 
 # 必要パッケージ
 emerge --verbose \
-  x11-base/xorg-drivers \
-  x11-base/xorg-server \
-  x11-terms/xterm \
-  x11-misc/tint2 \
-  x11-misc/picom \
-  lxde-base/lxappearance \
+  media-fonts/noto-cjk \
+  app-portage/gentoolkit \
   net-misc/networkmanager \
-  gnome-extra/nm-applet \
   app-misc/fastfetch \
   dev-vcs/git
 
 rc-update add NetworkManager default
-
-# ユーザー用ホームディレクトリに dotfiles クローン
-#sudo -u $USERNAME bash <<'INNER'
-#cd ~
-#git clone --depth 1 https://github.com/Benzenec6h6/dotfiles.git
-#cp -r dotfiles/gentoo_dot/. ~/
-#rm -rf dotfiles
-#INNER
-
-# xinitrc
-#echo "exec openbox-session" > /home/$USERNAME/.xinitrc
-
-# 権限の修正
-#chown -R $USERNAME:$USERNAME /home/$USERNAME
