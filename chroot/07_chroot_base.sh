1aec49c33d7c2aaca87c9146ad4ec01b66663d79
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-05-24 04:32:43 +0900

    Create a CPU optimization flag after synchronization

--- chroot/07_chroot_base.sh
+++ chroot/07_chroot_base.sh
@@ -15,6 +15,14 @@ echo 'keymap="jp106"' > /etc/conf.d/keymaps
 echo "127.0.0.1 localhost" > /etc/hosts
 echo "$HOSTNAME" > /etc/hostname
 
+# === グラフィックドライバー設定 ===
+echo "[+] Setting up video cards..."
+mkdir -p /etc/portage/package.use
+echo "*/* VIDEO_CARDS: -* virgl qxl gallium" > /etc/portage/package.use/00video_cards
+
+# === パッケージ同期 ===
+emerge-webrsync
+
 # === CPU最適化フラグの自動設定 ===
 echo "[*] Setting up CPU_FLAGS_X86..."
 
@@ -26,14 +34,6 @@ mkdir -p /etc/portage/package.use
 echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
 
 echo "[✓] CPU flags successfully configured: $(cpuid2cpuflags)"
-
-# === グラフィックドライバー設定 ===
-echo "[+] Setting up video cards..."
-mkdir -p /etc/portage/package.use
-echo "*/* VIDEO_CARDS: -* virgl qxl gallium" > /etc/portage/package.use/00video_cards
-
-# === パッケージ同期 ===
-emerge-webrsync
 
 # === プロファイル切り替え (コメント解除) ===
 echo "[+] Selecting desktop profile..."
